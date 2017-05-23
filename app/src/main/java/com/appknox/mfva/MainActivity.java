bf8be35ec3bd614cd51f51ebea6ac419880953c5
Viren Nadkarni <viren@outlook.com>
2017-05-23 13:59:09 +0530

    Add WebView and extra permissions

--- app/src/main/java/com/appknox/mfva/MainActivity.java
+++ app/src/main/java/com/appknox/mfva/MainActivity.java
@@ -1,13 +1,39 @@
 package com.appknox.mfva;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonApi, buttonWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Context context = this;
+
+        buttonApi = (Button) findViewById(R.id.buttonApi);
+        buttonApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("API", "Requesting");
+            }
+        });
+
+        buttonWebView = (Button) findViewById(R.id.buttonWebView);
+        buttonWebView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("WebView", "Opening");
+                Intent intent = new Intent(context, WebViewActivity.class);
+		        startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/appknox/mfva/WebViewActivity.java
@@ -0,0 +1,23 @@
+package com.appknox.mfva;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * Created by viren on 23/5/17.
+ */
+
+public class WebViewActivity extends Activity {
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://example.com");
+
+    }
+}
