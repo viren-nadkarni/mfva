60fe0594968cbf6c29f89781126fd356dbd2dd6e
Viren Nadkarni <viren@outlook.com>
2017-05-25 17:16:02 +0530

    Add vulnerabilities
    - Unsecured exported activity and service
    - Redis library
    - DES/ECB crypto handler
    - Dangerous custom permission

--- app/src/main/java/com/appknox/mfva/ExportedActivity.java
+++ app/src/main/java/com/appknox/mfva/ExportedActivity.java
@@ -1,8 +1,16 @@
 package com.appknox.mfva;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
+import redis.clients.jedis.Jedis;
 
 public class ExportedActivity extends AppCompatActivity {
 
@@ -11,5 +19,13 @@ public class ExportedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exported);
 
+        Log.d("redis", "Initialising jedis...");
+        Jedis jedis = new Jedis("localhost");
+
+        try {
+            Cipher.getInstance("DES/ECB/ZeroBytePadding", "BC");
+        } catch (NoSuchAlgorithmException|NoSuchProviderException|NoSuchPaddingException e) {
+            // pass
+        }
     }
 }
