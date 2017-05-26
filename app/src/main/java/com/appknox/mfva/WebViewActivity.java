02bdc33db945f50e1e5b8da0bb15cb41f57f78bb
Viren Nadkarni <viren@outlook.com>
2017-05-26 12:10:14 +0530

    Add ActionBar to WebView activity

--- app/src/main/java/com/appknox/mfva/WebViewActivity.java
+++ app/src/main/java/com/appknox/mfva/WebViewActivity.java
@@ -2,20 +2,26 @@ package com.appknox.mfva;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 
 /**
  * Created by viren on 23/5/17.
  */
 
-public class WebViewActivity extends Activity {
+public class WebViewActivity extends AppCompatActivity {
     private WebView webView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        webView = (WebView) findViewById(R.id.webView);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_api_requests);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.activity_webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://example.com");
     }
