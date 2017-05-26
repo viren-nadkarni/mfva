b2ba6961c1c0c339682700692c077b2f6e1b199d
Viren Nadkarni <viren@outlook.com>
2017-05-26 11:11:36 +0530

    Add vulnerabilities
    - Unprotected receiver
    - Exported service

---
+++ app/src/main/java/com/appknox/mfva/ExportedReceiver.java
@@ -0,0 +1,24 @@
+package com.appknox.mfva;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by viren on 26/5/17.
+ */
+
+public class ExportedReceiver extends BroadcastReceiver {
+    private static final String TAG = "ExportedReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Action: " + intent.getAction() + "\n");
+        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+        String log = sb.toString();
+        Log.d(TAG, log);
+        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+    }
+}
